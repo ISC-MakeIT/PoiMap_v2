283ebc475ca27bb26aa1a61ba5d3f44701d71e1c
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2019-11-16 20:23:23 +0900

    Added settings for dev-server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.join(__dirname, "build/")
+    path: path.join(__dirname, "build/"),
+    publicPath: "/js/",
+  },
+  devServer: {
+    inline: true,
+    watchContentBase: true,
+    contentBase: path.join(__dirname, 'public'),
+    open: true,
+    openPage: "index.html",
+    port : 3000,
   }
 };
