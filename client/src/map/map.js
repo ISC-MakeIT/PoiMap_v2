32bcf79edeeca31711ba0d0ebb3ce253cf1036e6
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-17 16:19:24 +0900

    :construction:ADD create dom element
    ~

--- client/src/map/map.js
+++ client/src/map/map.js
@@ -1,10 +1,39 @@
 class Map {
   constructor(markerInfo) {
     this.markerInfo = markerInfo;
-    this.target = document.getElementById('map');
+    this.target = document.querySelector('#navigation-page-link');
+    this.root = document.createElement('section');
+    this.root.id = '';
     this.map;
     this.onMarker = markerInfo;
     this.filterFlg = 0b0000;
+    this.category = [
+      // 変更の余地ありそう
+      {
+        Name: 'ペットボトル',
+        Id: 'sort-PETbottle',
+        Img: '../../svg/selectPlasticBottle-icon.svg',
+        Flg: 0b0001
+      },
+      {
+        Name: '缶・ビン',
+        Id: 'sort-bottleCan',
+        Img: '../../svg/selectPlasticBottle-icon.svg',
+        Flg: 0b0010
+      },
+      {
+        Name: '燃えないゴミ',
+        Id: 'sort-NonBurn',
+        Img: '../../svg/selectPlasticBottle-icon.svg',
+        Flg: 0b0100
+      },
+      {
+        Name: '燃えるゴミ',
+        Id: 'sort-CanBurn',
+        Img: '../../svg/selectPlasticBottle-icon.svg',
+        Flg: 0b1000
+      }
+    ];
   }
 
   init() {
@@ -12,6 +41,9 @@ class Map {
       alert('このブラウザーは対応していません');
       return;
     }
+    const mapSection = document.createElement('section');
+    mapSection.id = 'map';
+
     const success = pos => {
       const nowLat = pos.coords.latitude;
       const nowLng = pos.coords.longitude;
@@ -27,12 +59,43 @@ class Map {
           position: google.maps.ControlPosition.LEFT_BOTTOM
         }
       };
-      this.map = new google.maps.Map(this.target, mapOptions);
+      this.map = new google.maps.Map(mapSection, mapOptions);
+      // filterを作成
+      const nav = document.createElement('nav');
+      nav.id = 'filter-nav';
+      const ul = document.createElement('ul');
+      this.category.map(i => {
+        const li = document.createElement('li');
+        li.id = i.Id;
+        li.addEventListener('click', () => {
+          this.filterFlg ^= i.Flg;
+        });
+        const sortName = document.createElement('p');
+        sortName.id = `${i.Id}-txt`;
+        sortName.innerText = i.Name;
+        const sortIcon = document.createElement('img');
+        sortIcon.id = `${i.Id}-img`;
+        sortName.appendChild(imsortIcon);
+        li.appendChild(sortName);
+        ul.appendChild(li);
+      });
+      nav.appendChild(ul);
+      mapSection.append(nav);
     };
     const error = error => {
-      console.log(error);
+      section.innerHTML = '<p>GoogleMapを表示できません</p> ';
+      console.error(error);
     };
+    const header = document.createElement('header');
+    const backHomeIcon = document.createElement('img');
+    backHomeIcon.src = '../../svg/back.svg';
+    const h1 = document.createElement('h1');
+    h1.innerText = '現在位置';
+    header.appendChild(backHomeIcon);
+    header.appendChild(h1);
+
     navigator.geolocation.getCurrentPosition(success, error);
+    this.target.appendChild(section);
   }
 
   displayMarker() {
